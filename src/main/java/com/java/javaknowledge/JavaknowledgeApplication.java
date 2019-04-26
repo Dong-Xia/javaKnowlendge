23b00cbc232c13f6e7743cf91d732b3274c83225
xiadong <xiadong891204>
2019-04-26 16:03:17 +0800

    新建java知识积累工程

---
+++ src/main/java/com/java/javaknowledge/JavaknowledgeApplication.java
@@ -0,0 +1,13 @@
+package com.java.javaknowledge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaknowledgeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaknowledgeApplication.class, args);
+    }
+
+}
